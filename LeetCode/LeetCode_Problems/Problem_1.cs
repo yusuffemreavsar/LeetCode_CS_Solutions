00f3117ce125013f5e959ae7de59b77221f3f498
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-01-05 18:47:32 +0300

    LeetCode Problems.

---
+++ LeetCode/LeetCode_Problems/Problem_1.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Two Sum Problem
+ ------------------
+ Problem Link: https://leetcode.com/problems/two-sum/
+ Run Time: 133ms
+ Memory: 47.55 MB
+ ------------------
+*/
+
+namespace LeetCode.LeetCode
+{
+    internal class Problem_1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            int newValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newValue = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (newValue + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
