b3cd212c8d60b5fed1ed73f43e4131e9e52a8a08
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-03-23 03:59:53 +0300

    ..

--- LeetCode/LeetCode_Problems/Problem_1.cs
+++ LeetCode/LeetCode_Problems/Problem_1.cs
@@ -15,7 +15,7 @@ namespace LeetCode.LeetCode
         {
             int[] result = new int[2];
             int newValue;
-
+            int deneme2 = 20;
             for (int i = 0; i < nums.Length; i++)
             {
                 newValue = nums[i];
